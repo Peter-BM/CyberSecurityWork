7fe97b083392ffc209f3632c86231c3c144dc7ae
Koko <pedrobobella@gmail.com>
2024-11-23 17:15:27 -0300

    feat: Creating endpoint to signup.

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -14,9 +14,23 @@ class AuthController < ApplicationController
     end
   end
 
+  def signup
+    user = User.new(user_params)
+  
+    if user.save
+      render json: { message: 'User created successfully', user: user }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+  
   private
 
   def encode_token(payload)
     JWT.encode(payload, SECRET_KEY)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
